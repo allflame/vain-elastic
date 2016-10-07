3531e0cb06e22bdf0bf773f35172389243935e0f
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-10-07 14:25:08 +0300

    Initial commit

---
+++ src/Extension/ElasticExtension.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-amqp
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-amqp
+ */
+
+namespace Vain\Elastic\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class ElasticExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class ElasticExtension extends AbstractExtension
+{
+}
